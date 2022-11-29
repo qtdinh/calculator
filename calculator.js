0ce28435709754db5e5ccc33d45f3182848b1538
Thomas Dinh <thomas98dinh@gmail.com>
2022-11-29 13:41:13 -0800

    Add operations and ordering to function

--- calculator.js
+++ calculator.js
@@ -1,3 +1,16 @@
+let currentNumber = 0;
+
+let display = document.querySelector('#screen');
+let container = document.querySelector('.calculator-grid');
+const numValues = document.querySelectorAll('.number');
+const opValues = document.querySelectorAll('.operation');
+let equals = document.querySelector('.equals');
+let savedOperator = '';
+let firstNumber = 0;
+let waitingForSecondOperand = false;
+let reset = false;
+
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -14,7 +27,15 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
-function operate(operator, num1, num2) {
+function operate(operator) {
+    if(currentNumber == "")
+    return;
+
+    if (firstNumber !== "" && operator !== null) {
+    
+    let num1 = parseFloat(firstNumber);
+    let num2 = parseFloat(currentNumber);
+
   switch (operator) {
     case '+':
         return add(num1, num2);
@@ -24,7 +45,51 @@ function operate(operator, num1, num2) {
         return multiply(num1, num2);
     case '/':
         return divide(num1, num2);
-  }
+ 
+    }
+    }
 }
 
+
+numValues.forEach(number => {
+    number.addEventListener('click', function(event) {
+        if(reset) {
+            display.value = '';
+            reset = false;
+        }
+        display.value = display.value + event.currentTarget.value;
+        currentNumber = display.value;
+    });
+});
+
+opValues.forEach(operation => {
+    operation.addEventListener('click', function() {
+        //if second operand true, assign savedOperator the new operator
+        //assign the previous result to the first number
+        //compute the current number with the first number
+
+        if(savedOperator !== operation.value && waitingForSecondOperand) {
+            savedOperator = operation.value;
+            return;
+        }
+
+        if(!savedOperator) {
+        firstNumber = currentNumber;
+        } else if (savedOperator) {
+            display.value = operate(savedOperator);
+            firstNumber = display.value;
+        }
+        console.log(firstNumber);
+        console.log(currentNumber);
+        waitingForSecondOperand = true;
+        savedOperator = operation.value;
+        currentNumber = '', reset = true;
+    });
+});
+
+equals.addEventListener('click', function() {
+    display.value = operate(savedOperator);
+    savedOperator = '';
+    waitingForSecondOperand = false;
+});
 
